3e3e6ea71b1d886082f67ca5dbb7342231eb77ea
KodofishChang <kodofish@gmail.com>
2017-02-22 16:01:01 +0800

    先建立Version 1 Hello Controller

---
+++ Exercise/Controllers/Version1/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Exercise.Controllers.Version1
+{
+    public class HelloController : ApiController
+    {
+        public Message Get()
+        {
+            return new Message { Token = "Joey-v1", Signature = "91" };
+        }
+    }
+
+    public class Message
+    {
+        public string Signature { get; set; }
+        public string Token { get; set; }
+    }
+}
