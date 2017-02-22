978feb198e5143ff22bb93fce4dc153849f80cb7
KodofishChang <kodofish@gmail.com>
2017-02-22 17:31:32 +0800

    在Application Start 執行Autofac

--- Exercise/Global.asax.cs
+++ Exercise/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,13 @@ namespace Exercise
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutofacConfig.Initialise();
         }
     }
 }
