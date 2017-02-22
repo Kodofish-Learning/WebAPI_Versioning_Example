9e77e390ec8ef1439eea34eaf3bbf7b08f7192ce
KodofishChang <kodofish@gmail.com>
2017-02-22 16:02:33 +0800

    建立 Version 2 的 hello Controller

---
+++ Exercise/Controllers/Version2/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Exercise.Controllers.Version2
+{
+    public class HelloController : ApiController
+    {
+        public AnotherMessage Get()
+        {
+            return new AnotherMessage { NewToken = "Joey-v2", NewSignature = "91" };
+        }
+    }
+
+    public class AnotherMessage
+    {
+        public string NewToken { get; set; }
+
+        public string NewSignature { get; set; }
+    }
+}
