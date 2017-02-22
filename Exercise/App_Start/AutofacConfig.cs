82ac7d21546b9e0ed244bb9fc5ba2599fa2d2adf
KodofishChang <kodofish@gmail.com>
2017-02-22 17:31:00 +0800

    加入Autofac 來注入IApiExplorer, IHttpControllerSelector

---
+++ Exercise/App_Start/AutofacConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using SDammann.WebApi.Versioning;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Autofac.Integration.WebApi;
+
+namespace Exercise.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Initialise()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            var config = GlobalConfiguration.Configuration;
+            builder.RegisterWebApiFilterProvider(config);
+            builder.RegisterType<VersionedApiExplorer>().As<IApiExplorer>().WithParameter("configuration", config).InstancePerRequest();
+            builder.RegisterType<RouteVersionedControllerSelector>().As<IHttpControllerSelector>().WithParameter("configuration", config).InstancePerLifetimeScope();
+            //builder.RegisterType<AcceptHeaderVersionedControllerSelector>().As<IHttpControllerSelector>().WithParameter("configuration", GlobalConfiguration.Configuration);
+
+
+            IContainer container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
