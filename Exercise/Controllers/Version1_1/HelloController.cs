e6a2708e9ec711f8b0e662d66f3bde05e9cf749c
KodofishChang <kodofish@gmail.com>
2017-09-11 19:31:40 +0800

    加入 v1.1 版 ApiController 測試版本號碼是否能夠正確指定

---
+++ Exercise/Controllers/Version1_1/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace Exercise.Controllers.Version1_1
+{
+    public class HelloController : ApiController
+    {
+        /// <summary>
+        /// Gets this instance. Version 1
+        /// </summary>
+        /// <returns></returns>
+        public Message Get()
+        {
+            return new Message {Token = "v1.1", Signature = "91"};
+        }
+    }
+
+    public class Message
+    {
+        public string Signature { get; set; }
+        public string Token { get; set; }
+    }
+}
